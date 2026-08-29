1bba8008fd17046455dade6a1a9cdb3432c3ca4e
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-08-29 14:23:38 +0300

    Update section two
    Added section two, repsonsive for phone, tablet, desktop.

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,11 @@
+import data from "../src/opportunities";
 import BtnRequest from "./components/btnrequest";
 // import bgMobile from "./assets/bg-intro-mobile.svg";
 // import bgDesktop from "./assets/bg-intro-desktop.svg";
 import bgPhone from "./assets/bg-phones.svg";
-// import data from "./opportunities";
 
 export default function Content() {
-  //   console.log(data);
+  console.log(data);
   return (
     <main>
       <section className="mb-14 md:mb-18 flex flex-col gap-12">
@@ -31,7 +31,7 @@ export default function Content() {
       </section>
       <section className="bg-gray-100 py-16 px-6 md:p-20">
         <div className="flex flex-col">
-          <div className="mb-12 md:mb-18 text-center">
+          <div className="mb-12 md:mb-18 text-center xl:text-left xl:w-158">
             <h2 className="preset-3 md:preset-2 text-blue-950 mb-4 md:mb-8">
               Why choose Digitalbank?
             </h2>
@@ -40,7 +40,19 @@ export default function Content() {
               financial hub. Control your finances like never before.
             </p>
           </div>
-          <div></div>
+          <div className="grid gap-y-8 md:grid-cols-2 xl:grid-cols-4 md:gap-y-16 md:gap-x-14 xl:gap-x-9 ">
+            {data.map((item) => (
+              <article className="flex flex-col items-center justify-center text-center md:text-left md:items-start">
+                {item.icon}
+                <p className="preset-4 text-blue-950 mt-10 mb-6">
+                  {item.header}
+                </p>
+                <p className="preset-7-r md:preset-6 md:leading-[162.5%] text-gray-600">
+                  {item.text}
+                </p>
+              </article>
+            ))}
+          </div>
         </div>
       </section>
     </main>
